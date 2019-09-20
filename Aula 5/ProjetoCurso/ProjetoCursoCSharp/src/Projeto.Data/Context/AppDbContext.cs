0ab0d23ca0a49e0e881cf2761feff4bfef566151
Scheffer <vscheffer@narwalsistemas.com.br>
2019-09-20 17:49:22 -0300

    Aula 5

---
+++ Aula 5/ProjetoCurso/ProjetoCursoCSharp/src/Projeto.Data/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Projeto.Data.Migrations;
+
+namespace Projeto.Data.Context
+{
+    [DbConfigurationType(typeof(NpgsqlConfiguration))]
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyConnection")
+        {
+            Database.SetInitializer<AppDbContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties().Where(a => a.Name.Equals("Codigo")).Configure(p => p.IsKey());
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
